0ef07d97b4f10299712d5ba1b6f4a140725524d7
Andrew Evsugin <ondrya@yandex.ru>
2021-02-21 12:37:37 +0300

    Add project files.

---
+++ CustomerDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplicitExplicitOperator
+{
+    /// <summary>
+    /// DTO class for a Web API return
+    /// </summary>
+    internal class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
